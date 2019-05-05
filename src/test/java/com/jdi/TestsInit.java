b4fabafb49e712047c631b51daff25653004e6aa
RomanIovlev <Roman_Iovlev@epam.com>
2019-05-05 21:12:02 +0300

    small corrections

--- src/test/java/com/jdi/TestsInit.java
+++ src/test/java/com/jdi/TestsInit.java
@@ -1,7 +1,11 @@
 package com.jdi;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+
+import static java.lang.Runtime.getRuntime;
 import static jdisite.pages.JDISite.homePage;
 import static jdisite.utils.DriverUtils.runChromeDriver;
 
@@ -10,5 +14,9 @@ public interface TestsInit {
     static void setUp() {
         runChromeDriver();
         homePage.open();
+    }
+    @AfterSuite(alwaysRun = true)
+    static void teardown() throws IOException {
+        getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
     }
 }
