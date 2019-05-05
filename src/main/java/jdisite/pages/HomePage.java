ab4da152cef40a8993ab88f0c2196e75e3c52301
RomanIovlev <Roman_Iovlev@epam.com>
2019-05-05 23:48:24 +0300

    add states

--- src/main/java/jdisite/pages/HomePage.java
+++ src/main/java/jdisite/pages/HomePage.java
@@ -3,27 +3,9 @@ package jdisite.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import static jdisite.entities.DefaultData.ROMAN;
-import static jdisite.pages.JDISite.loginForm;
-
 public class HomePage extends BasePage {
     public HomePage() { super("https://jdi-testing.github.io/jdi-light/"); }
     @FindBy(id ="user-icon") public WebElement userIcon;
     @FindBy(id ="user-name") public WebElement userName;
     @FindBy(css = ".fa-sign-out") public WebElement logout;
-
-    public void loggedOut() {
-        if (userName.isDisplayed()) {
-            if (!logout.isDisplayed())
-                userIcon.click();
-            logout.click();
-        }
-    }
-    public void loggedIn() {
-        if (!userName.isDisplayed()) {
-            if (loginForm.isHidden())
-                userIcon.click();
-            loginForm.loginAs(ROMAN);
-        }
-    }
 }
--- src/test/java/com/jdi/tests/ContactForm.java
+++ src/test/java/com/jdi/tests/ContactForm.java
@@ -4,7 +4,9 @@ import com.jdi.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static com.jdi.states.State.loggedIn;
 import static com.jdi.test.data.DefaultDataProvider.FULL_CONTACT;
+import static com.jdi.test.data.DefaultDataProvider.SIMPLE_CONTACT;
 import static jdisite.enums.MenuOptions.ContactForm;
 import static jdisite.pages.ContactPage.contactForm;
 import static jdisite.pages.JDISite.*;
@@ -12,14 +14,20 @@ import static jdisite.pages.JDISite.*;
 public class ContactForm implements TestsInit {
     @BeforeMethod
     public void before() {
-        homePage.loggedIn();
+        loggedIn();
         selectInMenu(ContactForm);
     }
 
     @Test
-    public void submitSeleniumContactFormTest() {
+    public void submitContactFormTest() {
         contactPage.open();
         contactForm.submit(FULL_CONTACT);
         contactForm.check(FULL_CONTACT);
+    }
+    @Test
+    public void submitContactFormSimpleTest() {
+        contactPage.open();
+        contactForm.submit(SIMPLE_CONTACT);
+        contactForm.check(SIMPLE_CONTACT);
     }
 }
